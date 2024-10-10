7cc58fe651ef8ef4217a0874f7b4ead19c905332
Marcus Nathan Dantas da Silva <144623168+nathandantas-qa@users.noreply.github.com>
2024-10-10 02:14:40 -0300

    Update login.cy.js

--- cypress/e2e/simple/login.cy.js
+++ cypress/e2e/simple/login.cy.js
@@ -3,9 +3,9 @@
 describe('Login no Mantis', () => {
   it('deve logar com sucesso', () => {    
     cy.visit('https://mantis-prova.base2.com.br/login_page.php');
-    cy.get('#username').should('be.visible').type(Cypress.env('USERNAME'));
+    cy.get('#username').should('be.visible').type(Cypress.env('CYPRESS_USERNAME'));
     cy.get('.width-40').click({ force: true });
-    cy.get('#password').should('be.visible').type(Cypress.env('PASSWORD'), { log: false });
+    cy.get('#password').should('be.visible').type(Cypress.env('CYPRESS_PASSWORD'), { log: false });
     cy.get('.width-40').click({ force: true });   
     cy.url({ timeout: 10000 }).should('include', 'my_view_page.php');
   })
