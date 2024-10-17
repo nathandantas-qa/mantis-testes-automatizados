7051a8fa06679cbbb36f799716728ec77dfa3d41
Marcus Nathan Dantas da Silva <144623168+nathandantas-qa@users.noreply.github.com>
2024-10-17 08:23:55 -0300

    Revert "Develop"

--- cypress/support/models/task-data.js
+++
@@ -1,9 +0,0 @@
-class TaskData {
-    constructor(category, summary, description) {
-      this.category = category;
-      this.summary = summary;
-      this.description = description;
-    }
-  }
-  
-  module.exports = TaskData;
